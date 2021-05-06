1f1651790537c220ddff46b36ca0c205b6238a10
samanmohammadraouf <www.saman.raouf@gmail.com>
2021-05-06 11:27:23 +0430

    all the

---
+++ math-chart-ds-finalVersion/MathChart(new)-ds - final version/MathChart(new)-ds/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace MathChart_new__ds
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea5 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend5 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series5 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.FinalCalbtn = new System.Windows.Forms.Button();
+            this.StepByStepCalBtn = new System.Windows.Forms.Button();
+            this.textBoxUp = new System.Windows.Forms.TextBox();
+            this.textBoxDown = new System.Windows.Forms.TextBox();
+            this.labelX = new System.Windows.Forms.Label();
+            this.textBoxequation = new System.Windows.Forms.TextBox();
+            this.textBoxData = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // chart1
+            // 
+            chartArea5.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea5);
+            legend5.Name = "Legend1";
+            this.chart1.Legends.Add(legend5);
+            this.chart1.Location = new System.Drawing.Point(12, 12);
+            this.chart1.Name = "chart1";
+            series5.ChartArea = "ChartArea1";
+            series5.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
+            series5.Legend = "Legend1";
+            series5.Name = "xy";
+            this.chart1.Series.Add(series5);
+            this.chart1.Size = new System.Drawing.Size(778, 585);
+            this.chart1.TabIndex = 0;
+            this.chart1.Text = "chart1";
+            // 
+            // FinalCalbtn
+            // 
+            this.FinalCalbtn.Location = new System.Drawing.Point(1191, 605);
+            this.FinalCalbtn.Name = "FinalCalbtn";
+            this.FinalCalbtn.Size = new System.Drawing.Size(165, 40);
+            this.FinalCalbtn.TabIndex = 1;
+            this.FinalCalbtn.Text = "محاسبه نهایی";
+            this.FinalCalbtn.UseVisualStyleBackColor = true;
+            this.FinalCalbtn.Click += new System.EventHandler(this.FinalCalbtn_Click);
+            // 
+            // StepByStepCalBtn
+            // 
+            this.StepByStepCalBtn.Location = new System.Drawing.Point(1191, 651);
+            this.StepByStepCalBtn.Name = "StepByStepCalBtn";
+            this.StepByStepCalBtn.Size = new System.Drawing.Size(165, 40);
+            this.StepByStepCalBtn.TabIndex = 2;
+            this.StepByStepCalBtn.Text = "محاسبه گام به گام";
+            this.StepByStepCalBtn.UseVisualStyleBackColor = true;
+            this.StepByStepCalBtn.Click += new System.EventHandler(this.StepByStepCalBtn_Click);
+            // 
+            // textBoxUp
+            // 
+            this.textBoxUp.Location = new System.Drawing.Point(253, 672);
+            this.textBoxUp.Name = "textBoxUp";
+            this.textBoxUp.Size = new System.Drawing.Size(88, 22);
+            this.textBoxUp.TabIndex = 3;
+            // 
+            // textBoxDown
+            // 
+            this.textBoxDown.Location = new System.Drawing.Point(100, 672);
+            this.textBoxDown.Name = "textBoxDown";
+            this.textBoxDown.Size = new System.Drawing.Size(95, 22);
+            this.textBoxDown.TabIndex = 4;
+            // 
+            // labelX
+            // 
+            this.labelX.AutoSize = true;
+            this.labelX.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelX.Location = new System.Drawing.Point(201, 672);
+            this.labelX.Name = "labelX";
+            this.labelX.Size = new System.Drawing.Size(46, 25);
+            this.labelX.TabIndex = 5;
+            this.labelX.Text = "<x<";
+            // 
+            // textBoxequation
+            // 
+            this.textBoxequation.Location = new System.Drawing.Point(100, 623);
+            this.textBoxequation.Name = "textBoxequation";
+            this.textBoxequation.Size = new System.Drawing.Size(241, 22);
+            this.textBoxequation.TabIndex = 6;
+            // 
+            // textBoxData
+            // 
+            this.textBoxData.Location = new System.Drawing.Point(796, 12);
+            this.textBoxData.Multiline = true;
+            this.textBoxData.Name = "textBoxData";
+            this.textBoxData.ReadOnly = true;
+            this.textBoxData.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBoxData.Size = new System.Drawing.Size(560, 587);
+            this.textBoxData.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(377, 620);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 25);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "تابع با x کوچک";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(384, 673);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 25);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "بازه";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1382, 720);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxData);
+            this.Controls.Add(this.textBoxequation);
+            this.Controls.Add(this.labelX);
+            this.Controls.Add(this.textBoxDown);
+            this.Controls.Add(this.textBoxUp);
+            this.Controls.Add(this.StepByStepCalBtn);
+            this.Controls.Add(this.FinalCalbtn);
+            this.Controls.Add(this.chart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button FinalCalbtn;
+        private System.Windows.Forms.Button StepByStepCalBtn;
+        private System.Windows.Forms.TextBox textBoxUp;
+        private System.Windows.Forms.TextBox textBoxDown;
+        private System.Windows.Forms.Label labelX;
+        private System.Windows.Forms.TextBox textBoxequation;
+        private System.Windows.Forms.TextBox textBoxData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ math-chart-ds-finalVersion/MathChart(new)-ds - final version/MathChart(new)-ds/Form1.cs
@@ -0,0 +1,511 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace MathChart_new__ds
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void FinalCalbtn_Click(object sender, EventArgs e)
+        {
+            
+            if(textBoxequation.Text=="")
+            {
+                MessageBox.Show("لطفا تابع را وارد کنید");
+            }
+            else if(textBoxUp.Text==""|| textBoxDown.Text=="")
+            {
+                MessageBox.Show("لطفا حدود تابع را وارد کنید");
+            }
+            else if(double.Parse(textBoxUp.Text)<double.Parse(textBoxDown.Text))
+            {
+                MessageBox.Show("حد بالا باید بیشتر از حد پایین باشد");
+            }
+            else
+            {
+                string equation = textBoxequation.Text;
+                double HighLimit = double.Parse(textBoxUp.Text);
+                double LowLimit = double.Parse(textBoxDown.Text);
+                Placement(equation, HighLimit, LowLimit);
+
+            }
+            
+        }
+        private void Placement(string equation, double HighLimit, double LowLimit)
+        {
+            double x = LowLimit;
+            int count = (int)Math.Floor((HighLimit - LowLimit) * 2);
+            count = count + 1;
+            Expression mathChartEquation = new Expression();
+            string[] EquationArray = mathChartEquation.MakeArray(equation);
+            string[] secondEquationArray = new string[EquationArray.Length];
+            for (int i = 0; i < EquationArray.Length; i++)
+            {
+                secondEquationArray[i] = EquationArray[i];
+            }
+            double[] y = new double[count];
+            for (int j = 0; j < count; j++)
+            {
+                for (int i = 0; i < EquationArray.Length; i++)
+                {
+                    if (EquationArray[i] == "x")
+                    {
+                        EquationArray[i] = x.ToString();
+
+                    }
+                    if (i == EquationArray.Length - 1)
+                    {
+                        textBoxData.Text = textBoxData.Text + "\n" + "for x = " + x.ToString() +Environment.NewLine;
+                        y[j] = mathChartEquation.Arzyabi(mathChartEquation.MianvandToPasvand(EquationArray));
+                    }
+                }
+                x = x + 0.5;
+                for (int i = 0; i < EquationArray.Length; i++)
+                {
+                    EquationArray[i] = secondEquationArray[i];
+                }
+            }
+            x = LowLimit;
+            chart1.Series["xy"].Points.Clear();
+            for (int i = 0; i < y.Length; i++)
+            {
+                this.chart1.Series["xy"].Points.AddXY(x, y[i]);
+                x = x + 0.5;
+            }
+            
+        }
+        private void Placement(string equation , double HighLimit ,double LowLimit , int timeDealy)
+        {
+            double x = LowLimit;
+            int count = (int)Math.Floor((HighLimit - LowLimit) * 2);
+            count = count + 1;
+            Expression mathChartEquation = new Expression();
+            string [] EquationArray = mathChartEquation.MakeArray(equation);
+            string[] secondEquationArray = new string[EquationArray.Length];
+            for(int i=0;i<EquationArray.Length;i++)
+            {
+                secondEquationArray[i] = EquationArray[i];
+            }
+            double[] y = new double[count];
+            for(int j =0;j<count;j++)
+            {
+                for (int i = 0; i < EquationArray.Length; i++)
+                {
+                    if (EquationArray[i] == "x")
+                    {
+                        EquationArray[i] = x.ToString();
+                        
+                    }
+                    if(i==EquationArray.Length-1)
+                    {
+                        textBoxData.Text = textBoxData.Text + Environment.NewLine + "for x = " + x.ToString()+Environment.NewLine;
+                        y[j] = mathChartEquation.Arzyabi(mathChartEquation.MianvandToPasvand(EquationArray,timeDealy,textBoxData),timeDealy,textBoxData);
+                    }
+                }
+                x = x + 0.5;
+                for(int i=0;i<EquationArray.Length;i++)
+                {
+                    EquationArray[i] = secondEquationArray[i];
+                }
+            }
+            x = LowLimit;
+            for(int i=0;i<y.Length;i++)
+            {
+                this.chart1.Series["xy"].Points.AddXY(x, y[i]);
+                x = x + 0.5;
+            }
+            
+        }
+
+        private void StepByStepCalBtn_Click(object sender, EventArgs e)
+        {
+            int timeDelay = 1000;
+            if (textBoxequation.Text == "")
+            {
+                MessageBox.Show("لطفا تابع را وارد کنید");
+            }
+            else if (textBoxUp.Text == "" || textBoxDown.Text == "")
+            {
+                MessageBox.Show("لطفا حدود تابع را وارد کنید");
+            }
+            else if (double.Parse(textBoxUp.Text) < double.Parse(textBoxDown.Text))
+            {
+                MessageBox.Show("حد بالا باید بیشتر از حد پایین باشد");
+            }
+            else
+            {
+                string equation = textBoxequation.Text;
+                double HighLimit = double.Parse(textBoxUp.Text);
+                double LowLimit = double.Parse(textBoxDown.Text);
+                Placement(equation, HighLimit, LowLimit,timeDelay);
+
+            }
+        }
+    }
+    public class stack<T>
+    {
+        static public int size = 100;
+        public int top = -1;
+        
+        public T[] elements = new T[size];
+        //constructor dar in ja mana nadarad chon khod barname bayad pishfarz size bedahad.
+        //public stack(int x )
+        //{
+
+        //}
+
+        public T peek()
+        {
+            if (top == -1)
+                throw new EmtyException();
+            return elements[top];
+        }
+        public void Push(T element)
+        {
+            if (top == size - 1)
+            {
+                throw new FullStackException();
+            }
+            top++;
+            elements[top]=element;
+        }
+        public T Pop()
+        {
+            T element = peek();
+            top--;
+            return element;
+        }
+        public Boolean IsEmpty()
+        {
+            return -1 == top;
+        }
+        public Boolean IsFull()
+        {
+            return top == size - 1;
+        }
+    }
+
+    public class Expression
+    {
+        public string[] MakeArray(string text)
+        {
+            char[] array = text.ToCharArray();
+            string[] numbers = text.Split('+', '-','*','/','^');
+            List<string> Operations = new List<string>();
+            for(int i=0;i<array.Length;i++)
+            {
+                if(array[i]=='+'|| array[i] == '-' || array[i] == '/' || array[i] == '*' || array[i] == '^' )
+                {
+                    Operations.Add(array[i].ToString());
+                }
+            }
+            string[] EquationArray = new string[numbers.Length + Operations.Count];
+            int length = 0;
+            int count = 0;
+            for(int i=0;i<EquationArray.Length;i++)
+            {
+                if((i+1)%2==1)
+                {
+                    EquationArray[i] = numbers[length];
+                    
+                    if(length<numbers.Length-1)
+                        length++;
+                }
+                else
+                {
+                    EquationArray[i] = Operations[count];
+                    
+                    if(count<Operations.Count-1)
+                        count++;
+                }
+            }
+            
+            return EquationArray;
+        }
+        public bool Pre(string a, string b)
+        {
+            if ((a == "+" || a == "-") && (b == "*" || b == "/" || b == "^" || b == "!" ))
+            {
+                return false;
+            }
+            else if ((a == "/" || a == "*") && ( b == "^" || b == "!"))
+            {
+                return false;
+            }
+            else if ((a == "!") && (b == "^"))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public List<string> MianvandToPasvand(string [] array)
+        {
+            
+            stack<string> Operations = new stack<string>();
+            for(int i =0; i<array.Length;i++)
+            {
+                if(array[i].Contains("!"))
+                {
+                    string[] numbers = array[i].Split('!');
+                    int number = int.Parse(numbers[0]);
+                    int product = 1;
+                    for(int j=1;j<=number;j++)
+                    {
+                        product = product * j;
+                    }
+                    array[i] = product.ToString();
+                }
+            }
+            
+            List<string> answer = new List<string>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != "+" && array[i] != "-" && array[i] != "*" && array[i] != "/" && array[i] != "^")
+                {
+                    answer.Add(array[i]);
+                }
+                else
+                {
+                    l1:
+                    if (Operations.top != -1)
+                    {
+                        if (Pre(Operations.elements[Operations.top], array[i]))
+                        {
+                            
+                            string obj = Operations.Pop();
+                            answer.Add(obj.ToString());
+                            goto l1;
+                        }
+                        else
+                        {
+                            Operations.Push(array[i]);
+                        }
+                    }
+
+                    
+                    else
+                    {
+                        Operations.Push(array[i]);
+                    }
+                }
+                
+            }
+            while(Operations.IsEmpty()==false)
+            {
+                string character = Operations.Pop();
+                answer.Add(character);
+            }
+            return answer;
+        }
+
+        public List<string> MianvandToPasvand(string[] array, int timeDelay, TextBox textboxData)
+        {
+            
+            stack<string> Operations = new stack<string>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Contains("!"))
+                {
+                    string[] numbers = array[i].Split('!');
+                    int number = int.Parse(numbers[0]);
+                    int product = 1;
+                    for (int j = 1; j <= number; j++)
+                    {
+                        product = product * j;
+                    }
+                    array[i] = product.ToString();
+                }
+            }
+            
+            List<string> answer = new List<string>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] != "+" && array[i] != "-" && array[i] != "*" && array[i] != "/" && array[i] != "^")
+                {
+                    answer.Add(array[i]);
+                }
+                else
+                {
+                    l1:
+                    if (Operations.top != -1)
+                    {
+                        if (Pre(Operations.elements[Operations.top], array[i]))
+                        {
+                            string obj = Operations.Pop();
+                            answer.Add(obj.ToString());
+                            goto l1;
+                        }
+                        else
+                        {
+                            Operations.Push(array[i]);
+                        }
+                    }
+                    
+                    else
+                    {
+                        Operations.Push(array[i]);
+                    }
+                }
+                textboxData.Text = textboxData.Text+ Environment.NewLine + "Pasvandi:";
+                for (int x = 0; x < answer.Count; x++)
+                {
+                    textboxData.Text = textboxData.Text + answer[x] + " ";
+                }
+                textboxData.Text = textboxData.Text + Environment.NewLine + "OperationStack:";
+                for (int x = 0; x < Operations.elements.Length; x++)
+                {
+                    textboxData.Text = textboxData.Text + Operations.elements[x] + " ";
+                }
+                textboxData.Text = textboxData.Text + Environment.NewLine;
+                Application.DoEvents();
+                Thread.Sleep(timeDelay);
+            }
+            while (Operations.IsEmpty() == false)
+            {
+                
+                string character = Operations.Pop();
+                answer.Add(character);
+            }
+            textboxData.Text = textboxData.Text + Environment.NewLine + "Final Pasvandi:" ;
+            for(int x =0;x<answer.Count;x++)
+            {
+                textboxData.Text = textboxData.Text + answer[x] + " ";
+            }
+            textboxData.Text = textboxData.Text + Environment.NewLine;
+            
+            Application.DoEvents();
+            Thread.Sleep(timeDelay);
+            return answer;
+        }
+
+        public double Arzyabi(List<string> array)
+        {
+            stack<double> mohasebe = new stack<double>();
+
+            
+            for(int i=0;i<array.Count;i++)
+            {
+                if(array[i]=="+"|| array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "^" || array[i] == "!")
+                {
+                    double a = mohasebe.Pop();
+                    double b = mohasebe.Pop();
+                    double c = 0;
+                    switch (array[i])
+                    {
+                        case "+":
+                            c = b + a;
+                            break;
+                        case "-":
+                            c = b - a;
+                            break;
+                        case "*":
+                            c = b * a;
+                            break;
+                        case "/":
+                            c = b / a;
+                            break;
+                        case "^":
+                            c = Math.Pow(b,a);
+                            break;
+                        //case ! be sorat jodagne handle shode.
+                        
+
+                    }
+                    mohasebe.Push(c);
+                }
+                else
+                {
+                    double x = double.Parse(array[i].ToString());
+                    mohasebe.Push(x);
+                }
+            }
+
+            double answer = mohasebe.Pop();
+            return answer;
+        }
+
+        public double Arzyabi(List<string> array, int timeDelay, TextBox textboxData)
+        {
+            stack<double> mohasebe = new stack<double>();
+            
+            for (int i = 0; i < array.Count; i++)
+            {
+                if (array[i] == "+" || array[i] == "-" || array[i] == "*" || array[i] == "/" || array[i] == "^" || array[i] == "!")
+                {
+                    double a = mohasebe.Pop();
+                    double b = mohasebe.Pop();
+                    double c = 0;
+                    switch (array[i])
+                    {
+                        case "+":
+                            c = b + a;
+                            break;
+                        case "-":
+                            c = b - a;
+                            break;
+                        case "*":
+                            c = b * a;
+                            break;
+                        case "/":
+                            c = b / a;
+                            break;
+                        case "^":
+                            c = Math.Pow(b, a);
+                            break;
+                            //case '!':
+
+
+                    }
+                    mohasebe.Push(c);
+                }
+                else
+                {
+                    double x = double.Parse(array[i].ToString());
+                    mohasebe.Push(x);
+                }
+                textboxData.Text = textboxData.Text + "CalculateStack:";
+                for (int x = 0; x <= mohasebe.top; x++)
+                {
+                    textboxData.Text = textboxData.Text + mohasebe.elements[x] + " ";
+                }
+                textboxData.Text = textboxData.Text + Environment.NewLine;
+                Application.DoEvents();
+                Thread.Sleep(timeDelay);
+            }
+
+            double answer = mohasebe.Pop();
+            return answer;
+        }
+    }
+    public class EmtyException : Exception
+    {
+        public EmtyException()
+        {
+            MessageBox.Show("the stack is empty", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+
+    public class FullStackException : Exception
+    {
+        public FullStackException()
+        {
+            MessageBox.Show("the stack is full", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+    }
+    
+}
